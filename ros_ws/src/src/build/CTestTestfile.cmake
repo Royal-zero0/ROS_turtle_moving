1ee48d9010d71251a62fd8919c5c18873b3d99d8
Royal-zero0 <67706999+Royal-zero0@users.noreply.github.com>
2024-11-09 23:26:38 +0530

    Add files via upload

---
+++ ros_ws/src/src/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zero/ros_ws/src/src
+# Build directory: /home/zero/ros_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_session")
